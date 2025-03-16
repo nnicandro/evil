6b64a38a62c2726bcf4cc55fd8819a1035c9698f
Nathaniel Nicandro <2367246+nnicandro@users.noreply.github.com>
2025-03-16 14:34:33 -0500

    Update evil-jumps.el
    Remove mistaken author attribution.

--- evil-jumps.el
+++ evil-jumps.el
@@ -1,8 +1,6 @@
 ;;; evil-jumps.el --- Jump list implementation  -*-
 
 ;; Author: Nathaniel Nicandro <nathanielnicandro at gmail.com>
-;; Original Author: Bailey Ling <bling at live.ca>
-;;
 
 ;; Version: 1.15.0
 
